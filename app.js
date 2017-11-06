ea260c86f4d5cacd6450b53139a136e400105862
Carlos Sagala <carlos.s@opendevsolutions.com.ar>
2017-11-06 18:15:22 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(3000, () => console.log('ejemplo escuchando en el puerto 3000!'));
